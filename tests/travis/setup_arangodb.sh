fd2168bcbebd647f9ebbfec872e1a3637714e64d
frank <frank@frankmayer.net>
2016-09-18 20:00:35 +0300

    AAARGH! v2

--- tests/travis/setup_arangodb.sh
+++ tests/travis/setup_arangodb.sh
@@ -9,8 +9,8 @@ NAME="ArangoDB-${VERSION}"
 
 if [ ! -d "$DIR/$NAME" ]; then
   # download ArangoDB
-  echo "wget http://www.arangodb.org/repositories/travisCI/$NAME.tar.gz"
-  wget http://www.arangodb.org/repositories/travisCI/${NAME}.tar.gz
+  echo "wget http://www.arangodb.org/repositories/nightly/travisCI/$NAME.tar.gz"
+  wget http://www.arangodb.org/repositories/nightly/travisCI/${NAME}.tar.gz
   echo "tar zxf $NAME.tar.gz"
   tar zvxf ${NAME}.tar.gz
 fi
